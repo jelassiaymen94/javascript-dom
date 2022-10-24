56900b2ef10189a5d5ecc4c79ffdca94b3f8ec9f
Jelassi Aymen <nhedber1@students.solano.edu>
2022-10-24 09:57:01 +0100

    Level 2 Done

--- main.js
+++ main.js
@@ -21,3 +21,27 @@ document.querySelector("#paragraph").id = "paragraphId";
 document.querySelector("#paragraph1").className = "paragraphClass";
 document.querySelector("#paragraph2").setAttribute("id", "paragraphId2");
 document.querySelector("#paragraph3").setAttribute("class", "paragraphClass2");
+
+// Change stye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
+document.querySelector("#paragraphId").style.color = "red";
+document.querySelector("#paragraph1").style.backgroundColor = "blue";
+document.querySelector("#paragraphId2").style.border = "1px solid black";
+document.querySelector("#paragraph3").style.fontSize = "20px";
+document.querySelector("#paragraph3").style.fontFamily = "Arial";
+
+// Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color
+
+paragraphs.forEach((paragraph, index) => {
+	if (index % 2 === 0) {
+		paragraph.style.color = "green";
+	} else {
+		paragraph.style.color = "red";
+	}
+});
+
+// Set text content, id and class to each paragraph
+paragraphs.forEach((paragraph, index) => {
+	paragraph.textContent = `I am paragraph ${index}`;
+	paragraph.id = `paragraphId${index}`;
+	paragraph.className = `paragraphClass${index}`;
+});
