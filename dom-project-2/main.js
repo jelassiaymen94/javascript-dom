b2d4e5ee7ce265a81c37c4964ea4d348504163cb
Jelassi Aymen <nhedber1@students.solano.edu>
2022-10-24 15:39:30 +0100

    exercice 2 done

---
+++ dom-project-2/main.js
@@ -0,0 +1,30 @@
+/* Create a div container on HTML document and create 100 to 100 numbers dynamically and append to the container div. 
+Even numbers background is green
+Odd numbers background is yellow
+Prime numbers background is red */
+
+// Path: dom-project-2/main.js
+/* Create a div container on HTML document and create 100 to 100 numbers dynamically and append to the container div. 
+Even numbers background is green
+Odd numbers background is yellow
+Prime numbers background is red */
+
+const container = document.createElement("div");
+container.setAttribute("class", "container");
+document.body.appendChild(container);
+
+for (let i = 1; i <= 100; i++) {
+	let div = document.createElement("div");
+	div.setAttribute("class", "div");
+	div.style.width = "150px";
+	div.innerHTML = parseInt(Math.random() * i);
+	container.appendChild(div);
+
+	if (div.innerHTML % 2 == 0) {
+		div.style.backgroundColor = "green";
+	} else if (i % 2 != 0) {
+		div.style.backgroundColor = "yellow";
+	} else {
+		div.style.backgroundColor = "red";
+	}
+}
