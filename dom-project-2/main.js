59b5f5d701b03a13d6112791ac5e3ef7a39626d7
Jelassi Aymen <nhedber1@students.solano.edu>
2022-10-24 16:35:40 +0100

    cleaned up code

--- dom-project-2/main.js
+++ dom-project-2/main.js
@@ -3,12 +3,6 @@ Even numbers background is green
 Odd numbers background is yellow
 Prime numbers background is red */
 
-// Path: dom-project-2/main.js
-/* Create a div container on HTML document and create 100 to 100 numbers dynamically and append to the container div. 
-Even numbers background is green
-Odd numbers background is yellow
-Prime numbers background is red */
-
 const container = document.createElement("div");
 container.setAttribute("class", "container");
 document.body.appendChild(container);
